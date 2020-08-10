d70ec28a66fa38081c91fc59566901eb1985af6e
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2020-08-10 16:46:41 +0530

    Breadcrumbs Done

--- src/commissions/ClientPanal.jsx
+++ src/commissions/ClientPanal.jsx
@@ -2,22 +2,70 @@ import React, { useContext, useEffect, useState } from 'react'
 import {SalesContext, AuthenticationContext} from '../context'
 const ClientPanal = () => {
     const {salesperson_id} = useContext(AuthenticationContext)
-    const {connectedSalespersonList, salesperson, getSalespersonDetail} = useContext(SalesContext)
-    const [currSalesperson, setCurrSalesperson] = useState({
-        id: salesperson.id ? salesperson.id : "",
-        name: salesperson.name ? salesperson.name : ""
-    })
-    const [breadcrumbs, setBreadcrumbs] = useState()
+    const {connectedSalespersonList, getConnectedSalesperson, salesperson, getSalespersonDetail} = useContext(SalesContext)
+    const [breadcrumbs, setBreadcrumbs] = useState([])
     // const {salesperson_id} = useContext(AuthenticationContext)
     // Intial salesperson list
     useEffect(() =>{
         if(salesperson_id){
             getSalespersonDetail(salesperson_id)
-            // getConnectedSalesperson(salesperson_id)
+            // getConnectedSalesperson(salesperson_id)        
         }
     }, [salesperson_id])
 
+    useEffect(() => {
+        if(salesperson){
+            let tempList = breadcrumbs
+            const item = {
+                id: salesperson.id,
+                name: salesperson.name
+            }
+            tempList.push(item)
+            setBreadcrumbs(tempList)
+            getConnectedSalesperson(item.id) 
+        }
+    }, [salesperson])
+
     // HandleBreadcrumbs
+    const handleRowOnClick = (event) => {
+        // let tempList = breadcrumbs
+        // tempList.push(currSalesperson)
+        // setBreadcrumbs(tempList)
+        // console.log(event.currentTarget.getAttribute("data-id"))
+        // console.log(event.currentTarget.getAttribute("data-name"))
+        let tempList = breadcrumbs
+        const item = {
+            id: event.currentTarget.getAttribute("data-id"),
+            name: event.currentTarget.getAttribute("data-name")
+        }
+        tempList.push(item)
+        setBreadcrumbs(tempList)
+        getConnectedSalesperson(item.id)
+    }
+
+    const handleOnClickBreadCrumb = (event) => {
+        let tempList = breadcrumbs
+        const data = {
+            id: event.currentTarget.getAttribute("data-id"),
+            name: event.currentTarget.getAttribute("data-name")
+        }
+        let itemIndex = -1
+        tempList.forEach((item, index) => {
+            if(item.id === data.id){
+                itemIndex = index
+            }
+        })
+        
+        if(itemIndex <= 0){
+            tempList.splice(1) 
+        }else{
+            tempList.splice(itemIndex+1) 
+        }
+             
+        setBreadcrumbs(tempList)
+        getConnectedSalesperson(data.id)
+    }
+
     return (
         <div className="container">
             {salesperson && <>
@@ -51,8 +99,13 @@ const ClientPanal = () => {
             <div className="row mt-4">
                 <div className="col">
                     <div className="list mb-4">
-                        <div className="list-item">
-                            have a breadcrumb trail of salesperson here
+                        <div className="list-item d-flex">
+                            {/* Add Breadcrumbs Here */}
+                            {breadcrumbs.map((item, index) => {
+                                return (
+                                <div className="bredcrumb-item" key={index} onClick={handleOnClickBreadCrumb} data-id={item.id} data-name={item.name}>{item.name}</div>
+                                )
+                            })}                            
                         </div>
                         <div className="list-item">
                             <div className="row">
@@ -72,7 +125,7 @@ const ClientPanal = () => {
                         </div>
                         {connectedSalespersonList.map((item, index) =>{
                             return (
-                                <div className="list-item" key={index}>
+                                <div className="list-item" key={index} onClick={handleRowOnClick} data-id={item.id} data-name={item.name}>
                                     <div className="row">
                                         <div className="col-3">
                                             <p>{item.id}</p>
