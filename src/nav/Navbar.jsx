40b71b79fa712d05e5cb7ea857f74ac4ba5502cc
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2020-08-14 10:29:59 +0530

    Made Navbar Responsive

--- src/nav/Navbar.jsx
+++ src/nav/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
 
     const {role, username, logOut} = useContext(AuthenticationContext)
     const [classname, setClassname] = useState("nav-dropdown display-none")
+    const [dropdown, setDropdown] = useState(false)
     const history = useHistory()
 
     const handleUserOnClick = () => {
@@ -16,41 +17,80 @@ const Navbar = () => {
         }        
     }
 
+    const handleHambergurOnClick = () => {
+        setDropdown(!dropdown)
+    }
+
     const handleRedirectHome = () => {
         if(role === "ADMIN"){
             history.push("/admin/")
         }else if(role === "CLIENT"){
             history.push("/client/")
         }
+        setDropdown(false)
     }
 
     const handleAddAdmins = () => {
         history.push("/admin/register/admin")
+        setDropdown(false)
     }
 
     const handleSettings = () => {
         history.push("/user/register/settings")
+        setDropdown(false)
     }
 
     return (
         <nav className="nav">
-            <div className="container d-flex ">
-                <div className="nav-item  ml-auto" onClick={handleRedirectHome}>
-                    Home
-                </div>
-                { role === "ADMIN" && 
-                    <div className="nav-item" onClick={handleAddAdmins}>
-                        Create an Admin
+            <div className="container d-flex-a ">
+                {/* Add the logo here */}
+                {/* Hamburger Menu */}
+                <div className="ml-auto hamberger-menu">
+                    <div className="nav-item-head" onClick={handleHambergurOnClick}>
+                        Ham
                     </div>
-                }
-                <div className="nav-item nav-user" onClick={handleUserOnClick}>
-                    {username}
-                    <div className={classname}>
-                        <div className="nav-dropdown-item" onClick={handleSettings}>
-                            Settings
+                    {dropdown && 
+                    <div className="nav-hamberger">
+                        <div className="nav-item" onClick={handleRedirectHome}>
+                            Home
                         </div>
-                        <div className="nav-dropdown-item" onClick={logOut}>
-                            Logout
+                        { role === "ADMIN" && 
+                            <div className="nav-item" onClick={handleAddAdmins}>
+                                Create an Admin
+                            </div>
+                        }    
+                        <div className="nav-item nav-user" onClick={handleUserOnClick}>
+                            {username}
+                            <div className={classname}>
+                                <div className="nav-dropdown-item" onClick={handleSettings}>
+                                    Settings
+                                </div>
+                                <div className="nav-dropdown-item" onClick={logOut}>
+                                    Logout
+                                </div>
+                            </div>
+                        </div>                                            
+                    </div>
+                    }
+                </div>
+                <div className="nav-nav d-flex-a ml-auto">
+                    <div className="nav-item" onClick={handleRedirectHome}>
+                        Home
+                    </div>
+                    { role === "ADMIN" && 
+                        <div className="nav-item" onClick={handleAddAdmins}>
+                            Create an Admin
+                        </div>
+                    }
+                    <div className="nav-item nav-user" onClick={handleUserOnClick}>
+                        {username}
+                        <div className={classname}>
+                            <div className="nav-dropdown-item" onClick={handleSettings}>
+                                Settings
+                            </div>
+                            <div className="nav-dropdown-item" onClick={logOut}>
+                                Logout
+                            </div>
                         </div>
                     </div>
                 </div>
